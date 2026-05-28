729a67c6e0dfdb350b87724cb1d59da9d5e55061
Escosis <1906317507@qq.com>
2026-05-28 11:12:19 +0800

    v1.4.3: 优化地址栏

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.escosis.offlinewebview"
         minSdk = 24
         targetSdk = 36
-        versionCode = 12
-        versionName = "1.4.2"
+        versionCode = 13
+        versionName = "1.4.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
